7b0d19b368425723147dad04c6fcbc485fb5ec2b
liuzhao <1257625996@qq.com>
2024-03-28 00:29:32 +0800

    初始化项目

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+window.CESIUM_BASE_URL = 'cesium/'
+
+declare global {
+  interface Window {
+    CESIUM_BASE_URL: string
+  }
+}
+
+
 const app = createApp(App)
 
 app.use(router)
